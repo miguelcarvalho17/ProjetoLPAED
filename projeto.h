8f5d99325c47f36e518fcd49411620c45b63e988
António Pinto <58854197+AntonioPinto39825@users.noreply.github.com>
2020-12-19 14:54:47 +0000

    Add files via upload

--- projeto.h
+++ projeto.h
@@ -7,24 +7,65 @@
 #define MAX200 200
 
 
+typedef struct dias {
+    int dia;
+    int mes;
+    int ano;
+
+    struct evento *evento;
+} DIAS;
+
 typedef struct estudio {
-    int configuraçao; //numero de quartos
-
-
+    int id_estudio;
+    int custo;
+    int totalRegras;
+    int *codigo;
+    int totalDias;
+    int ocupaçao;
+    DIAS *array_dias;
 } ESTUDIO;
 
-
 typedef struct edificio {
-    char morada; // array
-    float latitude;
-    float longitude;
-    char name[MAX200];
-    ESTUDIO *pestudio;
-    int nestudios;
-} EDIFICIO;
+    int id_edificio;
+    int totalEstudios;
+    struct edificio *next;
+    ESTUDIO *array_estudio;
+}EDIFICIO ;
+
+typedef struct evento {
+    int id_evento, id_cliente;
+    char *nome_cliente;
+    char *nome_evento;
+    double horas_minutos;
+    struct evento *nextEvento;
+}EVENTO;
+
+typedef struct regras {
+    int id;
+    char *regra;
+    int preços;
+} REGRAS;
+
+typedef struct historicoEstadias {
+    int id_estadia;
+    char *nome_evento;
+    int preço;
+    double hora_minuto;
+    DIAS data;
+    struct historicoEstadias *pnext_estadia;
+} HISTORICOESTADIAS;
+
+typedef struct hospede{
+    int id_cliente;
+    char *nome_cliente;
+    HISTORICOESTADIAS *pprimeiraEstadia;
+    struct hospede *pnext;
+}HOSPEDE;
+
 
 
 void create_dynarray_edificio(EDIFICIO *pcs, int initsize);
+
 int main_projeto(int argc, const char *argv[]);
 
 #endif //PROJETOLPAED_PROJETO_H
